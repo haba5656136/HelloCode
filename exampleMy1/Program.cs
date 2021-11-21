8d7672f9533458bf719a074cd680388515bb303e
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-21 16:02:11 +0700

    По двум заданным числам проверять является ли первое квадратом второго

---
+++ exampleMy1/Program.cs
@@ -0,0 +1,6 @@
+﻿// выведение квадрата числа
+int numberA = new Random().Next(1,50);
+Console.WriteLine(numberA);
+Console.Write("квадрат числа = ");
+int result = numberA*numberA;
+Console.WriteLine(result);
---
+++ exampleMy2/Program.cs
@@ -0,0 +1,3 @@
+﻿int numberA = int.Parse(Console.ReadLine());
+double numberB = Math.Pow(numberA, 2.0);
+Console.Write($"Квадрат числа {numberA} = {numberB}");
