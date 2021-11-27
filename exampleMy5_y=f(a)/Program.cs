c97924d58e049076e2e3f5285dbd95deedef5605
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-27 18:38:02 +0700

    commit

---
+++ exampleMy5_y=f(a)/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine(" Написать программу вычисления значения функции y = f(a)");
+
+double f(double x);
+{
+    double result = x*x+1;
+    return result;
+}
+Console.WriteLine($"x = {result}");
