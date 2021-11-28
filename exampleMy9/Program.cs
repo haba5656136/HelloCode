bbeea328ecd0f8aa8f717bdf50c577dbb1096c5b
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-28 15:43:32 +0700

    9. Показать последнюю цифру трёхзначного числа

---
+++ exampleMy9/Program.cs
@@ -0,0 +1,9 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("введите трехзначное число: " );
+ 
+ int numberA = Convert.ToInt32(Console.ReadLine());
+
+ int numberB = numberA % 10;
+ 
+ Console.WriteLine($"последняя цифра в числе:{numberB} " );
