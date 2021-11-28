6dc9b56c16883cbe25c9e2279ad448354e82bd17
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-28 13:50:18 +0700

    доделал функцию задача 5

---
+++ exampleMy5_y=f(a)/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine(" Написать программу вычисления значения функции y = f(a) где f(a)=x*x+1");
+
+static double F(double x)
+    {
+         double result = (x*x+1);
+         return result;
+    }
+    Console.WriteLine("введите значение x: ");
+  
+  double x = Convert.ToDouble(Console.ReadLine());
+  Console.WriteLine($"y = {x}*{x}+1 = {F(x)}");
