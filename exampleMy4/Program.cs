a06bb849a1d42c5f5799915864766135ec36bec2
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-29 00:20:19 +0700

    исправил int.Parse на Convert.Toint32

--- exampleMy4/Program.cs
+++ exampleMy4/Program.cs
@@ -4,7 +4,7 @@ int a = new Random().Next(1, 10);
 
 Console.WriteLine("введите переменную b:");
 
-int b = int.Parse(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
 int c = 5;
 
