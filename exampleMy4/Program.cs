30f6989b8dc46710d908e564148619df35fa5269
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-28 23:34:11 +0700

    4. Найти максимальное из трех чисел

---
+++ exampleMy4/Program.cs
@@ -0,0 +1,18 @@
+﻿//4. Найти максимальное из трех чисел
+
+int a = new Random().Next(1, 10);
+
+Console.WriteLine("введите переменную b:");
+
+int b = int.Parse(Console.ReadLine());
+
+int c = 5;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
