506c8ef379fdd96a8036a52b96ad398e0dda5d1b
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-29 00:14:18 +0700

    11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ exampleMy11/Program.cs
@@ -0,0 +1,13 @@
+﻿//11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10, 99);
+
+int b = a % 10;
+int c = a/10;
+
+int max = b;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+ Console.WriteLine($"Наибольшая цифра числа: { max} " );
