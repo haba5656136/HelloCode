9b55c12358dc3042379aca7335eb7a784fc2ca7b
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-28 16:05:40 +0700

    10. Показать вторую цифру трёхзначного числа

---
+++ exampleMy10/Program.cs
@@ -0,0 +1,10 @@
+﻿//10. Показать вторую цифру трёхзначного числа
+Console.WriteLine("введите трехзначное число: " );
+ 
+ int numberA = Convert.ToInt32(Console.ReadLine());
+
+ int numberB = numberA % 100;
+ 
+ Console.WriteLine($"последняя цифра в числе:{numberB/10} " );
+
+
