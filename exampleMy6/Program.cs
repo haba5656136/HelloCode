f911d44da74e76e15662fafecfcf8da4ad280313
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-28 14:38:07 +0700

    выяснить является ли число четным задача 6

---
+++ exampleMy6/Program.cs
@@ -0,0 +1,17 @@
+﻿//6. Выяснить является ли число чётным
+                             
+Console.Write("введите число: ");
+
+int a= Convert.ToInt32(Console.ReadLine());
+ 
+if (a % 2==1)
+{
+    Console.WriteLine("число нечетное");
+           
+}
+else
+{
+    Console.WriteLine("Число четное");
+}
+ 
+ Console.ReadKey();
