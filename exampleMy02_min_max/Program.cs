c66cc6323fbb113774a769ad2d06dc17fb1d3f16
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-11-28 22:33:20 +0700

    2. Даны два числа. Показать большее и меньшее число

---
+++ exampleMy02_min_max/Program.cs
@@ -0,0 +1,14 @@
+﻿//  2. Даны два числа. Показать большее и меньшее число
+int a = 1;
+int b = 2;
+
+int max = a;
+int min = b;
+if (a > max) max = a; min = b;
+if (b > max) max = b; min = a;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
