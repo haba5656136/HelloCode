5a0018a3256ef5fd13adfafceb403d66bc72ed44
Хобовец Иван <haba5656136@gmail.com>
2021-12-04 12:18:00 +0700

    12.удалить вторую цифру трехзначного числа

--- exampleMy12/Program.cs
+++ exampleMy12/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿//12. Удалить вторую цифру трёхзначного числа
+Console.Write("введите трехзначное число: ");
+
+int a= Convert.ToInt32(Console.ReadLine());
+
+int number3 = a % 10;
+int number1 = a / 100;
+
+Console.WriteLine($"{number1}{number3}");
