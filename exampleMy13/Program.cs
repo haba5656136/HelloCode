813fadd2f9465881111eaf3d864faaab8687c727
Хобовец Иван <haba5656136@gmail.com>
2021-12-04 17:02:33 +0700

    18. Проверить истинность утверждения

---
+++ exampleMy13/Program.cs
@@ -0,0 +1,15 @@
+﻿//13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write( "введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 3;
+
+if (a % b == 0)
+{
+    Console.WriteLine($"{a} кратно 3");
+}
+else
+{
+    Console.WriteLine($"{a} не кратно 3 остаток = {a % b}");
+}
+
---
+++ exampleMy18/Program.cs
@@ -0,0 +1,17 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool[] x = { true, false };
+bool[] y = { true, false };
+int count = 0;
+
+for (int i = 0; i < x.Length; i++)
+{
+    for (int j = 0; j < y.Length; j++)
+    {
+        if ((!(x[i] || y[j])) == (!x[i] || !y[j])) count++;
+        Console.WriteLine($"{(!(x[i] || y[j])) == (!x[i] || !y[j])}");
+    }
+
+}
+if (count == 4) Console.WriteLine("Утверждение  ¬(X ⋁ Y) = ¬X ⋀ ¬Y истинно!");
+else Console.WriteLine("Утверждение  ¬(X ⋁ Y) = ¬X ⋀ ¬Y ложно!");
