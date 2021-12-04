a846b51578bde0677dfbe7801fe5e131c4037d4a
Хобовец Иван <haba5656136@gmail.com>
2021-12-04 18:06:55 +0700

    факториал

---
+++ factorial/Program.cs
@@ -0,0 +1,11 @@
+﻿// факториал
+
+double Faktorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Faktorial(n - 1);
+}
+for (int i = 0; i < 2; i++)
+{
+  Console.WriteLine($"{i}! = {Faktorial(i)}");  
+}
