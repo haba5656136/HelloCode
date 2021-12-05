29535ebe24b1f3c0b2ad4d98ea1b224475139e0e
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-05 23:07:47 +0700

    //28. Подсчитать сумму цифр в числе

---
+++ exampleMy28/Program.cs
@@ -0,0 +1,16 @@
+﻿//28. Подсчитать сумму цифр в числе
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int N = number.ToString().Length;
+int result = 0;
+int mod = 0;
+
+for (int i = 0; i < N; i++)
+{
+ 
+  mod = number % 10;
+  number = number / 10;
+  result = mod + result;   
+}
+Console.WriteLine($"сумма цифр в числе равна =  {result}");
