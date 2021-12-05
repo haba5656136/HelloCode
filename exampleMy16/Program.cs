db98a790e06eb06567a0ee452302e1efae549ccb
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-05 15:00:29 +0700

    //16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ exampleMy16/Program.cs
@@ -0,0 +1,36 @@
+﻿//16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+string numberWeekDay = Console.ReadLine();
+
+if(numberWeekDay.ToLower() == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+if(numberWeekDay.ToLower() == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if(numberWeekDay.ToLower() == "3")
+{
+    Console.WriteLine("Среда");
+}
+if(numberWeekDay.ToLower() == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if(numberWeekDay.ToLower() == "5")
+{
+    Console.WriteLine("Пятница");
+}
+if(numberWeekDay.ToLower() == "6")
+{
+    Console.WriteLine("Суббота");
+}
+if(numberWeekDay.ToLower() == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
+if(numberWeekDay.ToLower() == "6"|| numberWeekDay.ToLower() == "7")
+{
+    Console.WriteLine("выходной день!!!");
+}
