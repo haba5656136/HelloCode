c7312cd5ef7951058c2411eecad4d58ab8bf4062
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-05 19:50:15 +0700

    // 14. Найти третью цифру числа или сообщить, что её нет

---
+++ exampleMy14/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(x.ToString().Length);
+
+if (x.ToString().Length == 3)Console.WriteLine($"третья цифра числа {x} = {x % 10}");
+
+else if (x.ToString().Length == 4)Console.WriteLine($"третья цифра числа {x} = {x % 100/10}");
+
+else if (x.ToString().Length == 5)Console.WriteLine($"третья цифра числа {x} = {x % 1000/100}");
+
+else if (x.ToString().Length < 3)Console.WriteLine("число меньше трехзначного третьей цыфры нет!");
