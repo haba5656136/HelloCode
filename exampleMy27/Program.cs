9475822d3c708f82f8bbdcade50f0b60b8e9618b
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-06 07:59:36 +0700

    //27. Определить количество цифр в числе

---
+++ exampleMy27/Program.cs
@@ -0,0 +1,14 @@
+﻿//27. Определить количество цифр в числе
+
+Console.WriteLine("введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+while (x>0)
+{
+   x=x/10;
+   count++;
+}
+
+Console.WriteLine($"{count} цифр в числе");
