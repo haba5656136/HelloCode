eaea3fb6ad0bfe59d02423867356cd300aa225ea
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-11 17:38:18 +0700

    25. Найти сумму чисел от 1 до А

---
+++ exampleMy25/Program.cs
@@ -0,0 +1,12 @@
+﻿//25. Найти сумму чисел от 1 до А
+
+Console.WriteLine("введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    sum += i;
+
+}
+
+Console.WriteLine("сумма чисел от 1 до " + A +" = " + sum);
