fc1d79ee399e11bdf5f2d957ceaea322075dd9bc
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-11 14:29:13 +0700

    //30. Показать кубы чисел, заканчивающихся на четную цифру

---
+++ exampleMy30/Program.cs
@@ -0,0 +1,14 @@
+﻿//30. Показать кубы чисел, заканчивающихся на четную цифру
+
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number % 10 == 1 )
+{
+     Console.WriteLine("последнее число нечетное");
+}
+else 
+{
+    Console.WriteLine("последнее цифра числа четная, куб числа = " + Math.Pow(number,3));
+}
