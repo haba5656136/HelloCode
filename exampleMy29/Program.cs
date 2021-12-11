440db7f5e21b506c4e448bf3e3345484c81b3d6b
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-11 18:59:25 +0700

    //29. Написать программу вычисления произведения чисел от 1 до N

---
+++ exampleMy29/Program.cs
@@ -0,0 +1,14 @@
+﻿//29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+
+Console.WriteLine("N = " + N);
+
+for (int i = 1; i <= N; i++)
+{
+    a = i*a;
+    Console.WriteLine($"{i}*{a}");
+}
+Console.WriteLine("произведение всех чисел равно = " + a);
