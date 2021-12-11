9998d0dfff5844cdf9aabf0093421b6f13d6e20c
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-11 21:13:43 +0700

    35. Определить, присутствует ли в заданном массиве, некоторое число

---
+++ exampleMy35/Program.cs
@@ -0,0 +1,16 @@
+﻿//35. Определить, присутствует ли в заданном массиве, некоторое число 
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+int b = new Random().Next(1, 20);
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == b)
+    {
+        Console.WriteLine($"{b} присутствует в заданном массиве");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"{b} нет в заданном массиве");
+    }
+}
