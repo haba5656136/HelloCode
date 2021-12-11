d21c098e242996354146e48f3b8b180d9f602ef4
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-11 18:09:25 +0700

    //26. Возведите число А в натуральную степень B используя цикл

---
+++ exampleMy26/Program.cs
@@ -0,0 +1,13 @@
+﻿//26. Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите в какую степень надо возвести: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int C = A;
+
+for (int i = 1; i < B; i++)
+{
+    A *= A;// или A*A
+}
+Console.WriteLine($"{C} в степени {B} = {A}");
