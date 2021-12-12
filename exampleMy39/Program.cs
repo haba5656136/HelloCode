c6a0245c999bdbe501bcf1e3cbceb5e786d5705f
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-12 18:09:39 +0700

    /39. Найти произведение пар чисел в одномерном массиве.

---
+++ exampleMy39/Program.cs
@@ -0,0 +1,15 @@
+﻿//39. Найти произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Console.Clear();
+int[] array = new int[100];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i] + " ");
+}
+
+Console.WriteLine($"произведение первого и последнего числа массива : {array[0]} * {array[array.Length-1]} = {array[0]*array[array.Length-1]}");
+длину-1
+//доделать цикл произведений
