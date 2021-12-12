f687306050e0264f61cefce04a903b0988bc0dd1
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-12 15:59:10 +0700

    //37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]

---
+++ exampleMy37/Program.cs
@@ -0,0 +1,19 @@
+﻿//37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+Console.Clear();
+int[] array = new int[123];
+int col = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+for (int i = 10; i < 100; i++)
+{
+    col += 1;
+}
+Console.WriteLine("количество элементов равно " + col);
+
+
+
