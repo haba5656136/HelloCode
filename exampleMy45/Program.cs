fad0851e90967ca49e986d9d43b38b1f282a7b56
Хобовец Иван <haba5656136@gmail.com>
2021-12-18 16:13:17 +0700

    //45. Показать числа Фибоначчи

---
+++ exampleMy45/Program.cs
@@ -0,0 +1,11 @@
+﻿//45. Показать числа Фибоначчи
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 20; i++)
+{
+    Console.Write($"{Fibonacci(i)}, ");
+}
