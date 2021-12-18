a201739f6e7e8870642aa4200b0a81cee6ea7729
Хобовец Иван <haba5656136@gmail.com>
2021-12-18 17:08:27 +0700

    //42. Определить сколько чисел больше 0 введено с клавиатуры

---
+++ exampleMy42/Program.cs
@@ -0,0 +1,17 @@
+﻿//42. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("введите количество вводимых чисел : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[N];
+int sum = 0;
+Console.Write("введите числа :  ");
+for (int i = 0; i < N; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) sum = sum + 1;
+}
+Console.WriteLine("чисел больше ноля : " + sum);
