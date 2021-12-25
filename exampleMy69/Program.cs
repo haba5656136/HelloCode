5272e0dc35eed95ea3e5902957a2840c2b3653d9
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-25 18:45:06 +0700

    // 70. Найти сумму цифр числа

---
+++ exampleMy69/Program.cs
@@ -0,0 +1,16 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
+
+//int SumRec(int n)
+//{
+// if (n ==0) return 0;
+// else return n + SumRec(n - 1);
+//}
+//Console.WriteLine(SumRec(10)); сумма от 1 до n с лекции
+
+int SumRec(int m, int n)
+{
+    if (m < n)return m + SumRec(m + 1, n);
+    if (m > n) return m + SumRec(m - 1, n);
+    else return m;
+}
+Console.WriteLine(SumRec(5, 5));
---
+++ exampleMy70/Program.cs
@@ -0,0 +1,8 @@
+﻿// 70. Найти сумму цифр числа
+
+int SumNumber(int num)
+{
+    if (num>0) return num%10+SumNumber(num/10); 
+    return 0;
+}
+Console.WriteLine(SumNumber(0120));
