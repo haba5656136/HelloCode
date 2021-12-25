5656b00987891b0484adf6b98a3d246fe6a1c571
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-25 14:23:51 +0700

    // 66. Показать натуральные числа от 1 до N, N задано

---
+++ exampleMy66/Program.cs
@@ -0,0 +1,13 @@
+﻿// 66. Показать натуральные числа от 1 до N, N задано
+
+void Number(int n)
+{
+   
+    if (n != 1)
+    { 
+        Number(n - 1);
+    }
+     Console.Write($"{n} ");   
+}
+
+Number(10);
