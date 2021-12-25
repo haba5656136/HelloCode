628b6185795e8f213361fa483eb81f34c0484d15
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-25 19:14:47 +0700

    поправил 25 задачу

--- exampleMy25/Program.cs
+++ exampleMy25/Program.cs
@@ -1,12 +1,19 @@
 ﻿//25. Найти сумму чисел от 1 до А
 
-Console.WriteLine("введите число А: ");
-int A = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-for (int i = 1; i <= A; i++)
-{
-    sum += i;
+//Console.WriteLine("введите число А: ");
+//int A = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+//for (int i = 1; i <= A; i++)
+//{
+   /// sum += i;
 
-}
+//}
+//Console.WriteLine("сумма чисел от 1 до " + A +" = " + sum);
 
-Console.WriteLine("сумма чисел от 1 до " + A +" = " + sum);
+
+
+Console.Write("Введи A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+double sum = A * (A + 1) / 2;
+Console.Write("Сумма равна: ");
+Console.WriteLine(sum);
