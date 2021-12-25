9e6edc3a76cfa3327d11e557e942b58b3d69ffd0
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-25 14:30:37 +0700

    67. Показать натуральные числа от N до 1, N задано

---
+++ exampleMy67/Program.cs
@@ -0,0 +1,12 @@
+﻿// 67. Показать натуральные числа от N до 1, N задано
+
+void Number(int n)
+{
+    Console.Write($"{n} ");
+    if (n != 1)
+    {
+        Number(n - 1);
+    }
+
+}
+Number(10);
