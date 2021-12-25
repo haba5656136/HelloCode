96e13179605864f04a51246c41654305afbd8e9f
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-25 14:51:20 +0700

    добавил в 68 2ой способ

--- exampleMy68/Program.cs
+++ exampleMy68/Program.cs
@@ -1,9 +1,21 @@
 ﻿// 68. Показать натуральные числа от M до N, N и M заданы
 
-string NumberRec(int m,int n)
-{
-    if(m<=n) return NumberRec(m,n-1) + $"{n} ";
+//string NumberRec(int m,int n)
+//{
+    //if(m<=n) return NumberRec(m,n-1) + $"{n} ";
     //if(m<=n) return NumberRec(m+1,n) + $"{m} "; обратный отсчет!
-    else return string.Empty;
+    //else return string.Empty;
+//}
+//Console.WriteLine(NumberRec(1,10));
+
+void Number(int m,int n)
+{
+   
+    if (m<n)
+    { 
+        Number(m,n - 1);
+    }
+     Console.Write($"{n} ");   
 }
-Console.WriteLine(NumberRec(1,10));
+
+Number(3,10);
