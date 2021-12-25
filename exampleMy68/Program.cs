e820d503cfce54f1c30cc60a7a45950f3695ad7f
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-25 14:45:13 +0700

    // 68. Показать натуральные числа от M до N, N и M заданы

---
+++ exampleMy68/Program.cs
@@ -0,0 +1,9 @@
+﻿// 68. Показать натуральные числа от M до N, N и M заданы
+
+string NumberRec(int m,int n)
+{
+    if(m<=n) return NumberRec(m,n-1) + $"{n} ";
+    //if(m<=n) return NumberRec(m+1,n) + $"{m} "; обратный отсчет!
+    else return string.Empty;
+}
+Console.WriteLine(NumberRec(1,10));
