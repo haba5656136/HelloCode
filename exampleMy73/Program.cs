3cfa74c1e9ab1e60ce49cecd6b236aa80dc45960
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-26 14:59:06 +0700

    73. Написать программу показывающие первые N чисел

---
+++ exampleMy73/Program.cs
@@ -0,0 +1,14 @@
+﻿// 73. Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента 
+//последовательности задаются пользователем
+int N = 10;
+int Fibonacci(int first,int second,int n)
+{
+    if(n == 1) return first;
+    if(n == 2) return second;
+    return Fibonacci(first,second,n-1) + Fibonacci(first,second,n-2);
+}
+for (int i = 1; i <= N; i++)
+{
+   Console.Write($"{Fibonacci(10,28,i)}, "); 
+}
+    
